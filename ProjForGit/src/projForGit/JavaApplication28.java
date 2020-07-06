3bab0ed1e270729b5c084e05bc06c9e39562070d
Emil <Emil@Emil-PC>
2020-07-06 23:35:46 +0400

    Test11 yazildi

--- ProjForGit/src/projForGit/JavaApplication28.java
+++ ProjForGit/src/projForGit/JavaApplication28.java
@@ -5,7 +5,7 @@ public class JavaApplication28 {
    
     public static void main(String[] args) {
      
-        System.out.println("Hello GitPoj");
+        System.out.println("Test11");
     }
     
 }
